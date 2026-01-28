b822dc8a003ff870ba07168161454513123ad782
gaurav <singh.gaurav0052@gmail.com>
2026-01-28 22:09:03 +0530

    cors (rookie mistake)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.models.house_price.router import router as house_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="ML Inference API")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",   # local dev
+        "https://your-frontend-domain.vercel.app",  # later
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(house_router)
 
 @app.get("/")
